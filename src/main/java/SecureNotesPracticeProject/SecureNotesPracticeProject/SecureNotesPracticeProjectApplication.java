d957544a5f2ea601d6e838e25e2b784087cf4663
Vishal Choudhary <vishchy111@gmail.com>
2025-07-03 11:46:26 +0530

    Inital commit

---
+++ src/main/java/SecureNotesPracticeProject/SecureNotesPracticeProject/SecureNotesPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package SecureNotesPracticeProject.SecureNotesPracticeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureNotesPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureNotesPracticeProjectApplication.class, args);
+	}
+
+}
